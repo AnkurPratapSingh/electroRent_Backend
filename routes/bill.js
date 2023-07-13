e66934a465e748ac2758bd1c783c760e422cef1b
Ankur Pratap Singh <apratap@argusoft.com>
2023-07-13 18:23:54 +0530

    bill to pdf using html-pdf

--- routes/bill.js
+++ routes/bill.js
@@ -7,12 +7,43 @@ let pdf = require('html-pdf');
 let path = require('path');
 var fs = require('fs');
 var uuid = require('uuid');
-const auth = require("../services/authentication")
+const auth = require("../services/authentication");
+const { log } = require('console');
 
 
 router.post('/generateReport', auth.authenticationToken,(req,res)=>{
     const generateUuid = uuid.v1();
     const orderDetails = req.body;
-    var productDetailsReport = JSON.parse(orderDetails.productDeatils);
-   //gi var query = "insert into bill (name ,uuid," 
+    var productDetailsReport = JSON.parse(orderDetails.productDetails);
+   var query = "insert into bill (name ,uuid,email,contactNumber,paymentMethod,total,productDetails,createdBy) values (?,?,?,?,?,?,?,?)" ;
+   connection.query(query,[orderDetails.name,generateUuid,orderDetails.email,orderDetails.contactNumber,orderDetails.paymentMethod,orderDetails.totalAmount,orderDetails.productDetails,res.locals.email],(err,result)=>{
+    if(!err){
+        ejs.renderFile(path.join(__dirname,'',"report.ejs"),{productDetails:productDetailsReport,name:orderDetails.name,email:orderDetails.email,contactNumber:orderDetails.contactNumber,paymentMethod:orderDetails.paymentMethod,totalAmount:orderDetails.totalAmount},(err,result)=>{
+                    
+            if(err){
+                console.log("Hi this is render");
+                return res.status(500).json(err);
+
+            }
+            else{
+
+                pdf.create(result).toFile('./generated_pdf/'+generateUuid+".pdf",function(err,data){
+                    if(err){
+                    console.log(err);
+                    return res.status(500).json(err);
+                       }
+                    else{
+                        return res.status(200).json({uuid:generateUuid})
+                    }
+                })
+            }
+        })
+    }
+    else{
+        console.log("Hi");
+        return res.status(500).json(err);
+
+    }
+   });
 })
+module.exports = router;
